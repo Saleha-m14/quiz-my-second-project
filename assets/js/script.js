f6e1a59bbb4b45a2daea85e63da663b5d5ece46d
Saleha Mohammadi <saleha.moh2023@gmail.com>
2023-04-19 16:45:14 +0000

    add some styles and codes in javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,112 @@
-let questions = [
+const questions = [
     {
         question: "What is the meaning of eager?",
         answers: [
-            {reply: "exact", correct: false},
-            {reply: "aggressive", correct: false},
-            {reply: "right", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "sea", correct: false},
+              {text: "keen", correct: true},
+              {text: "south", correct: false},
+              {text: "response", correct: false},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of affect?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "infuence", correct: true},
+              {text: "quiet", correct: false},
+              {text: "risk", correct: false},
+              {text: "witch", correct: false},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of capital?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "city", correct: true},
+              {text: "object", correct: false},
+              {text: "center", correct: false},
+              {text: "explain", correct: false},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of silent?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "way", correct: false},
+              {text: "case", correct: false},
+              {text: "quite", correct: true},
+              {text: "idea", correct: false},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of Peace?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "way", correct: false},
+              {text: "hear", correct: true},
+              {text: "build", correct: false},
+              {text: "absence of war", correct: true},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of Except?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "course", correct: false},
+              {text: "leave out", correct: true},
+              {text: "loud", correct: false},
+              {text: "absence", correct: false},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of frigid?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
+              {text: "start", correct: false},
+              {text: "know", correct: false},
+              {text: "play", correct: false},
+              {text: "cool", correct: true},
+            ]
     },
     {
-        question: "What is the meaning of eager?",
+        question: "What is the meaning of slice?",
         answers: [
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "", correct: false},
-            {reply: "keen", correct: true},
-        ]
-    },
-
-
-];
-
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answers");
-const nextButton = document.getElementById("next-btn");
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-function testBegin(){
-    let currentQuestionIndex = 0;
-    let score = 0;
-    nextButton.innerHTML = "Next";
-    questionReveal();
-    
-}
-function questionReveal(){
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNumber = qcurrentQuestionIndex + 1;
-    questionElement.innerHTML = questionNumber + '. ', currentQuestion.question;
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.reply;
-        button.classList.add('btn');
-        answers.appendChild(button);
-        button.addEventListener("click", selectAnswer);
-    });
-}
-function resetState(){
-    nextButton.style.display = "none";
-    while(answerButtons.firstChild)
-    answerButtons.removeChild(answerButtons.firstChild);
-    
-}
-testBegin();
+              {text: "cut", correct: true},
+              {text: "count", correct: false},
+              {text: "destroy", correct: false},
+              {text: "carry", correct: false},
+            ]
+    }
+  
+  
+  ]
+  
+  const questionElement = document.getElementById("question");
+  const answerButtons = document.getElementById("answers");
+  const nextButton = document.getElementById("next-btn");
+  
+  let currentQuestionIndex = 0;
+  let score = 0;
+  
+  function startQuiz() {
+      currentQuestionIndex = 0;
+      score = 0;
+      nextButton.innerHTML = "Next";
+      showQuestion();
+  }
+  function showQuestion() {
+      // show question
+      let currentQuestion = questions[currentQuestionIndex];
+      let questionNo = currentQuestionIndex + 1;
+      questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+  
+      // show answers
+      currentQuestion.answers.forEach(answer => {
+          const button = document.createElement("button");
+          button.innerHTML = answer.text;
+          button.classList.add("btn");
+          answerButtons.appendChild(button);
+          if(answer.correct) {
+              button.dataset.correct = answer.correct;
+          }
+          button.addEventListener("click", selectAnswer);
+  
+        
+      });
+  
+  }
+  startQuiz();
